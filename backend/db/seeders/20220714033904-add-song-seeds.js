f59a7e7c31fea651149b7e5f2cda59304fbb1f77
tyler <dtctorrero@icloud.com>
2022-07-13 21:53:13 -0700

    added song seeds

--- backend/db/seeders/20220714033904-add-song-seeds.js
+++ backend/db/seeders/20220714033904-add-song-seeds.js
@@ -3,11 +3,25 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-    
-    await queryInterface.bulkInsert('People', [{
-       name: 'John Doe',
-       isBetaMember: false
-     }], {});
+
+    await queryInterface.bulkInsert('Songs', [
+      {
+       title: 'song1',
+       description: 'this is song1',
+       url: 'soundcloud.com/songs/song1',
+       previewImage: 'imageOfSong1.com',
+       albumId: 1,
+       userId: 1
+     },
+      {
+       title: 'song2',
+       description: 'this is song2',
+       url: 'soundcloud.com/songs/song2',
+       previewImage: 'imageOfSong2.com',
+       albumId: 2,
+       userId: 2
+     }
+    ], {});
 
   },
 
@@ -18,5 +32,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+     await queryInterface.bulkDelete('Songs', null, {});
   }
 };

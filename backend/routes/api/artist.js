c2ee51e37c72b1d0426034b51ed5957c76194f60
tyler <dtctorrero@icloud.com>
2022-07-21 19:22:22 -0700

    finished up to get artsts/id/albums

--- backend/routes/api/artist.js
+++ backend/routes/api/artist.js
@@ -3,9 +3,33 @@ const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth')
 const { Song, User, Album } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const app = require('../../app');
 
 const router = express.Router();
 
+const artistCouldNotBeFound = (next) => {
+    const e = new Error();
+    e.message = "Artist couldn't be found.";
+    e.status = 404;
+    next(e)
+}
 
-module.exports = router
+router.get('/:artistId/albums', async (req, res, next) => {
+    const { artistId } = req.params;
+
+    const artist = await User.getCurrentUserById(artistId);
+
+    if(artist){
+        const Albums = await artist.getAlbums({
+            where: {
+                userId: artist.id
+            }
+        })
+
+        return res.json({Albums});
+    }else{
+        artistCouldNotBeFound(next);
+    }
+})
+
+
+module.exports = router;

bc5f0d632899f80063f6f8cc84244657d5b876a3
tyler <dtctorrero@icloud.com>
2022-09-07 22:29:25 -0700

    fixed create album modal look

--- frontend/src/components/CreateAlbum/CreateAlbum.js
+++ frontend/src/components/CreateAlbum/CreateAlbum.js
@@ -2,6 +2,7 @@ import React, { useState} from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { createAlbum } from "../../store/albums";
+import './createalbum.css';
 
 
 const CreateAlbum = ({ setShowModal }) => {

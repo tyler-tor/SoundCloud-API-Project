6854ae4531cc00c6947b2131f4ad06fbc78b2ae3
tyler <dtctorrero@icloud.com>
2023-02-21 12:03:47 -0800

    finished user/album seeders

--- backend/db/seeders/20220702200935-demo-user.js
+++ backend/db/seeders/20220702200935-demo-user.js
@@ -17,20 +17,38 @@ module.exports = {
       email: 'demo@user.io',
       username: 'Demo-lition',
       hashedPassword: bcrypt.hashSync('password'),
-      previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/propic-6.jpg'
+      previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/propic-4.jpg',
+      id: 1
     },
     {
-      email: 'user1@user.io',
-      username: 'FakeUser1',
+      email: 'loslonleyboys@user.io',
+      username: 'Los Lonley Boys',
       hashedPassword: bcrypt.hashSync('password2'),
-      previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/1674670916129.jpg'
+      previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/propic-7.jpg',
+      id: 2
     },
     {
-      email: 'user2@user.io',
-      username: 'FakeUser2',
+      email: 'macmiller@user.io',
+      username: 'Mac Miller',
       hashedPassword: bcrypt.hashSync('password3'),
-      previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/1674669208418.jpg'
+      previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/propic-8.jpg',
+      id: 3
+    },
+    {
+      email: 'leonbridges@user.io',
+      username: 'Leon Bridges',
+      hashedPassword: bcrypt.hashSync('password3'),
+      previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/propic-5.jpg',
+      id: 4
+    },
+    {
+      email: 'kidcudi@user.io',
+      username: 'Kid Cudi',
+      hashedPassword: bcrypt.hashSync('password3'),
+      previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/propic-6.jpg',
+      id: 5
     }
+
    ], {})
   },
 
--- backend/db/seeders/20220702200938-add-album-seed.js
+++ backend/db/seeders/20220702200938-add-album-seed.js
@@ -13,16 +13,53 @@ module.exports = {
     options.tableName = 'Albums';
      await queryInterface.bulkInsert(options, [
       {
-       title: 'album1',
-       description: 'this is a description for album 1',
-       previewImage: 'album1.com',
-       userId: 1
+       title: 'Sacred',
+       description: "Sacred is the Los Lonely Boys' fourth album and their second studio set, released on July 18, 2006.",
+       previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/Sacred.jpg',
+       userId: 2,
+       id: 1
      },
       {
-       title: 'album2',
-       description: 'this is a description for album 2',
-       previewImage: 'album2.com',
-       userId: 2
+       title: 'Hyrbrid Theory',
+       description: 'Hybrid Theory is the debut studio album by American rock band Linkin Park, released on October 24, 2000',
+       previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/HyrbridTheory.jpg',
+       userId: 1,
+       id: 2
+     },
+      {
+       title: 'Meteora',
+       description: 'Meteora is the second studio album by American rock band Linkin Park. It was released on March 25, 2003',
+       previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/Linkin_Park_Meteora_Album_Cover.jpg',
+       userId: 1,
+       id: 3
+     },
+      {
+       title: 'Coming Home',
+       description: 'Coming Home is the debut studio album by American singer Leon Bridges. It was released on June 23',
+       previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/Coming_Home_Leon_Bridges.jpg',
+       userId: 4,
+       id: 4
+     },
+      {
+       title: 'K.I.D.S',
+       description: 'K.I.D.S. is the fourth mixtape by American rapper Mac Miller.',
+       previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/Mac_Miller_K.I.D.S._cover_art.jpg',
+       userId: 3,
+       id: 5
+     },
+      {
+       title: 'Circles',
+       description: 'Circles is the sixth and final studio album by American rapper and singer Mac Miller',
+       previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/Mac_Miller_-_Circles.jpg',
+       userId: 3,
+       id: 6
+     },
+      {
+       title: 'Entergalactic',
+       description: 'Entergalactic is the eighth studio album by American musician Kid Cudi. The album was issued on September 30, 2022',
+       previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/Kid_Cudi_-_Entergalactic.jpg',
+       userId: 5,
+       id: 7
      },
     ], {});
   },

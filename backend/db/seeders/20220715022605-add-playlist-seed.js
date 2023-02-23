a8c0e10e2cbf3f79bf23016c63855d98281d567f
tyler <dtctorrero@icloud.com>
2023-02-23 08:51:44 -0800

    very close to finishing seeders

--- backend/db/seeders/20220715022605-add-playlist-seed.js
+++ backend/db/seeders/20220715022605-add-playlist-seed.js
@@ -13,15 +13,80 @@ module.exports = {
     options.tableName = 'Playlists';
     await queryInterface.bulkInsert(options, [
       {
-        name: 'Playlist 1',
-        previewImage: 'playlist1.com',
+        name: 'Chill',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg2.jpg',
         userId: 1
       },
       {
-        name: 'Playlist 2',
-        previewImage: 'playlist2.com',
+        name: 'Summer Nights',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg12.jpg',
+        userId: 1
+      },
+      {
+        name: 'Plug Away',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg13.jpg',
+        userId: 1
+      },
+      {
+        name: 'Smile',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg14.jpg',
         userId: 2
-      }
+      },
+      {
+        name: 'Drive',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg15.jpg',
+        userId: 2
+      },
+      {
+        name: 'Take A Hike',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg10.jpeg',
+        userId: 2
+      },
+      {
+        name: 'China Town',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg8.jpg',
+        userId: 3
+      },
+      {
+        name: 'Game Away',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg6.jpg',
+        userId: 3
+      },
+      {
+        name: 'Weekend Hangout',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg4.jpg',
+        userId: 3
+      },
+      {
+        name: 'Lonely Adventure',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg5.jpg',
+        userId: 4
+      },
+      {
+        name: 'Beach Vibes',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg7.jfif',
+        userId: 4
+      },
+      {
+        name: 'Deep Thoughts',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg3.jpg',
+        userId: 4
+      },
+      {
+        name: 'Study Away',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg1.jpg',
+        userId: 5
+      },
+      {
+        name: 'The Future',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg9.jpg',
+        userId: 5
+      },
+      {
+        name: 'Lunch Time',
+        previewImage: 'https://soundcloud-bucket.s3.us-west-2.amazonaws.com/playlistimages/playlistimg11.jpg',
+        userId: 5
+      },
     ], {});
   },
 
--- backend/db/seeders/20220715024157-add-playlistSongs-seeds.js
+++ backend/db/seeders/20220715024157-add-playlistSongs-seeds.js
@@ -18,8 +18,604 @@ module.exports = {
      },
       {
        songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
+       playlistId: 1
+     },
+      {
+       songId: 2,
        playlistId: 2
      },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 2
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 3
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 4
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 5
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 6
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 7
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 8
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 9
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 10
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 11
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 12
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 13
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 14
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     },
+      {
+       songId: 2,
+       playlistId: 15
+     }
     ], {});
   },
 
